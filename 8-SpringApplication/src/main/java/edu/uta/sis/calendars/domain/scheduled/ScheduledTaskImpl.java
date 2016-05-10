db7fcc4f6d589c13f8897a21c3c5b9eeac9c9a3e
wwwprogramming <T5grwd?p1>
2016-05-10 12:57:18 +0300

    Scheduler example

---
+++ 8-SpringApplication/src/main/java/edu/uta/sis/calendars/domain/scheduled/ScheduledTaskImpl.java
@@ -0,0 +1,16 @@
+package edu.uta.sis.calendars.domain.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Hannu Lohtander on 10.5.2016.
+ */
+@Component
+public class ScheduledTaskImpl implements ScheduledTasks {
+
+    @Scheduled(initialDelay=1000, fixedRate=5000)
+    public void runForrestRun() {
+        System.out.println("Climbing to the tree");
+    }
+}
---
+++ 8-SpringApplication/src/main/java/edu/uta/sis/calendars/domain/scheduled/ScheduledTasks.java
@@ -0,0 +1,7 @@
+package edu.uta.sis.calendars.domain.scheduled;
+
+/**
+ * Created by Hannu Lohtander on 10.5.2016.
+ */
+public interface ScheduledTasks {
+}
